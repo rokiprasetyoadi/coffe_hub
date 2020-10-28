d03800d8b4213aafae26b1bd3626d2013c8e0f2c
Roki Prasetyo Adi <rokiprasetyoadi@gmail.com>
2020-10-28 12:18:30 +0700

    kelewat
    video tantang kami

--- application/views/front_end/profile/tentang_kami.php
+++ application/views/front_end/profile/tentang_kami.php
@@ -50,3 +50,20 @@
         </div>
     </div>
     -->
+
+    <div class="video_area video_bg overlay" style="margin-bottom: 1px;">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-12">
+                    <div class="video_wrap text-center">
+                        <h3>Enjoy Video</h3>
+                        <div class="video_icon">
+                            <a class="popup-video video_play_button" href="https://www.youtube.com/watch?v=f59dDEk57i0">
+                                <i class="fa fa-play"></i>
+                            </a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
